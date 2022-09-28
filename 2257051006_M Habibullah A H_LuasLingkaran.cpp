f42c16544e83de239d04ab20545962f51b887351
Habib <112567198+MuhammadHabibullahAlHikam@users.noreply.github.com>
2022-09-28 09:36:44 +0700

    Add files via upload

---
+++ 2257051006_M Habibullah A H_LuasLingkaran.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float luas,r,phi = 3.14;
+	cout<<"menghitung luas lingkaran\n";
+	cout<<"masukan jari jari : ";
+	cin>>r;
+	luas=phi*r*r ;
+	cout<<"luas lingkaran  dengan jari jari "<<r<<" cm adalah :"<<luas<<"cm2";
+	
+	return 0;
+}
